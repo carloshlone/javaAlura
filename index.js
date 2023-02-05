dca37a11d65210b55ebc10233b8619f3c15fc9f1
carlos henrique lopes <carloshlopes.1996@gmail.com>
2023-02-04 22:46:54 -0300

    "atualizaçao do codigo "

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import { Cliente } from './Cliente.js'
 import { ContaCorrente } from './ContaCorrente.js'
+import { ContaPoupanca } from './ContaPoupanca.js'
 
 const cliente1 = new Cliente('Ricardo', 11122233309)
-const cliente2 = new Cliente('Alice', 88822233309)
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1)
+const contaCorrenteRicardo = new ContaCorrente(0, cliente1, 1001)
 contaCorrenteRicardo.depositar(500)
-const conta2 = new ContaCorrente(1002, cliente2)
+contaCorrenteRicardo.sacar(100)
 
-let valor = 200
-
-console.log(ContaCorrente.numeroDeContas)
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
+contaPoupanca.sacar(10)
+console.log(contaPoupanca)
+console.log(contaCorrenteRicardo)
