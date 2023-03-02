6a314e1fda5c352410f7f94b461f770b05ece492
carlos henrique lopes <carloshlopes.1996@gmail.com>
2023-03-02 09:14:01 -0300

    aa

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { Gerente } from './funcionarios/Gerente.js'
 import { Diretor } from './funcionarios/Diretor.js'
 import { SistemaAutenticacao } from './SistemaAutenticacao.js'
 
-const diretor = new Diretor('Rogrido, 10000, 123456')
+const diretor = new Diretor('Rogrido, 10000, 123456789')
 diretor.cadastrarSenha('123456')
 const gerente = new Gerente('Ricardo, 5000 , 12378945601')
 gerente.cadastrarSenha('123')
